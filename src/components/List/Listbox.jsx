e9b2b90bde512af1d1a5a143e9d772f973426563
asif-munshi <asifmunshe2978@gmail.com>
2023-09-11 08:29:15 +0600

    updated_listbox

--- src/components/List/Listbox.jsx
+++ src/components/List/Listbox.jsx
@@ -4,14 +4,6 @@ import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-// const people = [
-//   { name: "Wade Cooper" },
-//   { name: "Arlene Mccoy" },
-//   { name: "Devon Webb" },
-//   { name: "Tom Cook" },
-//   { name: "Tanya Fox" },
-//   { name: "Hellen Schmidt" },
-// ];
 import people from "../../lib/munshidb.json";
 
 export default function ListBox() {
